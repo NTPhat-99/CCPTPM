cf3ffef172e2ddbe8216e0dae500079e94733a96
NTPhat-99 <72063596+NTPhat-99@users.noreply.github.com>
2021-04-03 13:49:17 +0700

    Revert "Dev 1"

--- Loging.php
+++ Loging.php
@@ -5,7 +5,5 @@
 <form>
       <p>Username</p>
       <input type="text" name="" placeholder="Enter Username">
-      <p>PassWord</p>
-      <input type="text" name = "" placeholder="Enter Password">
-      ,<button type="button" name="button">Lonin</button>
+            
 </form>
