a951edab2d11c0afe488f2443aea02eaaed9da61
nguyenthuanphat.it@gmail.com <Phat@gmail123>
2021-04-03 15:17:34 +0700

    b viet message thong bao

--- Loging.php
+++ Loging.php
@@ -1,8 +1,35 @@
 <?php
 
+
+if (isset($_POST["Button"])) {
+  $u_name=$_POST["txtname"];
+  $u_pass=$_POST["txtpass"];
+  $user = "admin";
+  $pass = "admin";
+    if($u_name == $user && $u_pass ==$pass)
+    {
+      //echo "khong thanh cong";
+      ?>
+        <script>alert('Ban Dang Nhap Thanh Cong');</script>
+
+        <?php
+
+
+
+    }else {
+      ?>
+        <script>alert('Sai Tai Khoan Hoac Mat Khau');</script>
+
+        <?php
+    }
+
+
+  // code...
+}
+
 ?>
 
-<form>
+<form method="post">
   <label class="lbUserName">
     <p>Username</p>
     <input type="text" name="txtname" placeholder="Enter Username">
@@ -12,5 +39,5 @@
     <input type="text" name = "txtpass" placeholder="Enter Password">
   </label>
   <button name="Button" type="submit">Login</button>
-      
+
 </form>
